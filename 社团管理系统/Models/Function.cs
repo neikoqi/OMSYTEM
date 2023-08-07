8be27d3e88c867d4c8dee6751ccdbe4ac7393fe0
neikoqi <107788823+neikoqi@users.noreply.github.com>
2023-08-07 15:19:27 +0800

    Add files via upload

---
+++ 社团管理系统/Models/Function.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace 社团管理系统.Models
+{
+    public class Function
+    {
+        private SqlCommand com;
+        private SqlConnection con;
+        private SqlDataAdapter sda;
+        //private DataTable dt;
+        private string constr;
+
+        public Function()
+        {
+            constr = "Data Source=DESKTOP-7O5IA5E\\SQLEXPRESS;Initial Catalog=MassOrganizaitions;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            con = new SqlConnection(constr);
+            com = new SqlCommand();
+            com.Connection = con;
+
+        }
+
+        public DataTable GetData(string Query)
+        {
+            DataTable dt = new DataTable();
+            sda = new SqlDataAdapter(Query, constr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(string Query)
+        {
+            int cnt = 0;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            com.CommandText = Query;
+            cnt = com.ExecuteNonQuery();
+            con.Close();
+            return cnt;
+        }
+
+
+    }
+
+}
+
---
+++ 社团管理系统/Models/LINQ.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+namespace 社团管理系统.Models
+{
+    public class LINQ
+
+    {
+        //数据库链接
+        public static string sql = "Data Source=DESKTOP-7O5IA5E\\SQLEXPRESS;Initial Catalog=MassOrganizaitions;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static SqlConnection sqlcon;
+
+
+        public static void sqln()
+        {
+            //链接状态判断
+            sqlcon = new SqlConnection(sql);
+            if (sqlcon.State == ConnectionState.Broken)
+            {
+                sqlcon.Open();
+                sqlcon.Close();
+            }
+            else if (sqlcon.State == ConnectionState.Closed)
+            {
+                sqlcon.Open();
+            }
+        }
+        public static SqlDataReader sqld(string sql)
+        {
+            sqln();
+            SqlCommand sqlcom = new SqlCommand(sql, sqlcon);
+            SqlDataReader sqlData = sqlcom.ExecuteReader();
+            return sqlData;
+        }
+
+
+    }
+}
