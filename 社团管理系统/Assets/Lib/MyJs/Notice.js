b52bd92204dcc9e3311b2a885dd768e37c066fd1
neikoqi <107788823+neikoqi@users.noreply.github.com>
2023-08-07 15:29:42 +0800

    Add files via upload

---
+++ 社团管理系统/Assets/Lib/MyJs/Notice.js
@@ -0,0 +1,107 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // 封装的动画函数 动画对象obj（obj要有定位）  移动的目标举例target ，返回函数callback;
+    function animation(obj, target, callback) {
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function () {
+            if (obj.offsetLeft == target) {
+                clearInterval(obj.timer);
+                //简写利用&& 的短路          
+                callback && callback();
+            }
+            var flag = (target - obj.offsetLeft) / 10;
+            var step = flag > 0 ? Math.ceil(flag) : Math.floor(flag);
+            obj.style.left = obj.offsetLeft + step + 'px';
+        }, 30);
+    }
+    //获取节点
+    var focus = document.querySelector('.focus');
+    var focus_l = focus.querySelector('.focus_l');
+    var focus_r = focus.querySelector('.focus_r');
+    var pic_ul = focus.querySelector('.pic_ul');
+    var pic_ol = document.querySelector('.focus_ol');
+    //焦点图的第一张的最后一张图是一样的，直接克隆节点
+    var pic_li = pic_ul.children[0].cloneNode(true);
+    pic_ul.appendChild(pic_li);
+    var circle = 0;
+    for (var i = 0; i < pic_ul.children.length - 1; i++) {
+        var ol_li = document.createElement('li');
+        pic_ol.appendChild(ol_li);
+        pic_ol.children[i].setAttribute('index', i);
+        // 点击小圆圈，可以播放相应图片。
+        pic_ol.children[i].addEventListener('click', function () {
+            for (var i = 0; i < pic_ol.children.length; i++) {
+                pic_ol.children[i].className = '';
+            }
+            this.className = 'current';
+            num = this.getAttribute('index')
+            circle = this.getAttribute('index');
+            animation(pic_ul, -this.getAttribute('index') * focus.offsetWidth);
+        })
+    }
+    // 鼠标不经过轮播图，轮播图也会自动播放图片 
+    var time = setInterval(function () {
+        focus_r.click();
+    }, 4000);
+    // 未点击小圆圈 ，默认选择给第一个加样式
+    pic_ol.firstElementChild.className = 'current';
+    //1.鼠标经过轮播图模块，左右按钮显示
+    focus.addEventListener('mouseenter', function () {
+        focus_l.style.display = 'block';
+        focus_r.style.display = 'block';
+        clearInterval(time); //鼠标经过，轮播图模块， 自动播放停止。
+        time = null;
+    });
+    //1.鼠标经过轮播图模块，离开隐藏左右按钮。
+    focus.addEventListener('mouseleave', function () {
+        focus_l.style.display = 'none';
+        focus_r.style.display = 'none';
+        time = setInterval(function () {
+            focus_r.click();
+        }, 4000);
+    });
+    var num = 0;
+    var flags = true; //节流阀 防止连续点击，造成轮播速度过快
+    //.点击右侧按钮一次，图片往左播放一张，以此类推，左侧按钮同理。
+    focus_r.addEventListener('click', function () {
+        if (flags) {
+            flags = false;
+            if (num == pic_ul.children.length - 1) {
+                num = 0;
+                pic_ul.style.left = '0px';
+            };
+            num++;
+            animation(pic_ul, -num * focus.offsetWidth, function () {
+                flags = true;
+            });
+            circle++;
+            if (circle == pic_ol.children.length) {
+                circle = 0;
+            }
+            for (var i = 0; i < pic_ol.children.length; i++) {
+                pic_ol.children[i].className = '';
+            }
+            pic_ol.children[circle].className = 'current';
+        }
+    });
+    focus_l.addEventListener('click', function () {
+        if (flags) {
+            flags = false;
+            if (num == 0) {
+                num = pic_ul.children.length - 1;
+                pic_ul.style.left = -num * focus.offsetWidth + 'px';
+            }
+            num--;
+            animation(pic_ul, -num * focus.offsetWidth, function () {
+                flags = true;
+            });
+            circle--;
+            if (circle < 0) {
+                circle = pic_ol.children.length - 1;
+            }
+            for (var i = 0; i < pic_ol.children.length; i++) {
+                pic_ol.children[i].className = '';
+            }
+            pic_ol.children[circle].className = 'current';
+        }
+    });
+})
---
+++ 社团管理系统/Assets/Lib/MyJs/rotation.js
@@ -0,0 +1,89 @@
+﻿window.onload = function () {
+	function getIdName(id, tagName) {
+		if (tagName != 0) {
+			return document.getElementById(id).getElementsByTagName(tagName);
+		}
+		else {
+			return document.getElementById(id);
+		}
+	}
+
+	function rotation(json) {
+		var that;
+		var start = 0;
+		var index = 0;
+		var timer = null;
+		var option = {
+			li: "li",		//默认用li包裹
+			wrapperId: "",	//最外面 id
+			imageId: "",		//图片外面id
+			buttonId: "",	//按钮外面id
+			ms: 4000			//多少毫秒切换一张,默认4000毫秒
+		}
+
+		for (var i in option) {
+			if (json[i] != undefined) {
+				option[i] = json[i];
+			}
+		}
+
+		var wrapper = getIdName(option.wrapperId, 0);
+		var images = getIdName(option.imageId, option.li);
+		var buttons = getIdName(option.buttonId, option.li);
+
+		function scrollImage(that) {
+			if (that >= 0) {
+				index = that;
+			}
+			else {
+				if (start == 0) {
+					index++;
+				}
+				else {
+					index--;
+					start = 0;
+				}
+			}
+
+			if (index >= buttons.length) index = 0;
+
+			if (index < 0) index = buttons.length - 1;
+
+			for (var j = 0; j < buttons.length; j++) {
+				images[j].className = "";
+				buttons[j].className = "";
+			}
+
+			images[index].className = "current";
+			buttons[index].className = "on";
+		}
+
+		images[index].className = "current";
+		buttons[index].className = "on";
+
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].index = i;
+
+			buttons[i].onclick = function () {
+				that = this.index;
+				scrollImage(that);
+			}
+		}
+
+		timer = setInterval(scrollImage, option.ms);
+
+		wrapper.onmouseover = function () {
+			clearInterval(timer);
+		}
+
+		wrapper.onmouseout = function () {
+			timer = setInterval(scrollImage, option.ms);
+		}
+	}
+
+	rotation({
+		wrapperId: "focus_wrapper",
+		imageId: "focus_image",
+		buttonId: "focus_button"
+	});
+}
